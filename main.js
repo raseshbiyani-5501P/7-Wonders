8156a3e6781f453c94f5233b47a8d462bc4af717
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2020-10-23 10:17:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function img1() {
+  window.alert("The name Taj Mahal means crown of palaces. Construction of the Taj Mahal took around 20 years, beginning around 1632 and finishing around 1653. The Taj Mahal is made of white marble. One of the Taj Mahal's most recognizable features is a large white dome that is often called an 'onion dome' due to its shape");
+}
+function img3(){
+  window.alert("Christ the Redeemer is the largest art deco statue in the world. It is 98 feet tall not including the 26 foot pedestal and the arms stretch to 92 feet wide. The statue weighs approximately 635 tonnes. Christ the Redeemer is located in the Tijuca Forest National Forest at the top of the Corcovado Mountain.");
+}
+function img2(){
+  window.alert("Built between 72 AD and 80 AD, the Colosseum consists of stone and concrete.Measuring 189 metres long, 156 metres wide and 50 metres in height the Colosseum is the largest amphitheater in the world.The Colosseum could seat around 50,000 spectators for a variety of events.");
+}
+function img4(){
+  window.alert("The Eiffel Tower is 320 metres 1050 feet in height and was the tallest man made structure in the world for 41 years before being surpassed by the Chrysler Building in New York. The Eiffel Tower is made of iron and weighs around 10000 tonnes.");
+}
+  function img5(){
+    window.alert("The statues full name is Liberty Enlightening the World.It was a gift from France, given to America in 1886.The head of the statue was displayed at the World's Fair in Paris in 1878.");
+  }
+    function img6(){
+      window.alert("Some interesting facts on how the pyramids were built Khufus pyramid known as the great pyramid of Giza is the oldest and largest rising at 481 feet 146 meters. Archeologists say it was the tallest structure in the world for about 3800 years. The sphinx is a 73.5-meter 241 ft.");
+    }
+  function img7(){
+    window.alert("The Leaning Tower of Pisa Torre pendente di Pisa is a bell tower in Pisa Italy.The Tower of Pisa is world famous for its prominent tilt to one side.The towers foundations were built on soft subsoil which had difficulty supporting the towers weight 14,500 ton");
+  }
